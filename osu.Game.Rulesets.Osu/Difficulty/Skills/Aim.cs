a87d13f19bb935e70cf1351db6ac25c59a842ca6
avelia-tr <demonsofavelia@gmail.com>
2025-01-23 05:46:40 +0100

    fix dt2

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -2,6 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using System.Collections.Generic;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Osu.Difficulty.Evaluators;
@@ -19,12 +20,17 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
             this.withSliders = withSliders;
         }
 
+        private readonly List<double> previousStrains = new List<double>();
+
         private readonly bool withSliders;
 
         private double currentStrain;
 
-        private double skillMultiplier => 25.18;
+        private double skillMultiplier => 65;
         private double strainDecayBase => 0.15;
+
+        private double strainIncreaseRate => 10;
+
 
         private double strainDecay(double ms) => Math.Pow(strainDecayBase, ms / 1000);
 
@@ -32,10 +38,40 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 
         protected override double StrainValueAt(DifficultyHitObject current)
         {
-            currentStrain *= strainDecay(current.DeltaTime);
-            currentStrain += AimEvaluator.EvaluateDifficultyOf(current, withSliders) * skillMultiplier;
+            double currentDifficulty = AimEvaluator.EvaluateDifficultyOf(current, withSliders) * skillMultiplier;
+            double priorDifficulty = highestPreviousStrain(current, current.DeltaTime);
 
-            return currentStrain;
+            double lCurrentStrain = getStrainValueOf(currentDifficulty, priorDifficulty);
+            previousStrains.Add(lCurrentStrain);
+
+
+            // Strain is at most 2x the difficulty of the note.
+            // We cap notes to this value to prevent jumps into easy bursts from getting too much.
+            return Math.Min(currentDifficulty + currentStrain, currentDifficulty * 2);
+        }
+        private double getStrainValueOf(double currentDifficulty, double priorDifficulty) => (priorDifficulty * strainIncreaseRate + currentDifficulty) / (strainIncreaseRate + 1);
+
+        private double highestPreviousStrain(DifficultyHitObject current, double time)
+        {
+            double hardestPreviousDifficulty = 0;
+            double cumulativeDeltaTime = time;
+
+            double timeDecay(double ms) => Math.Pow(strainDecayBase, Math.Pow(ms / 900, 7));
+
+            for (int i = 0; i < previousStrains.Count; i++)
+            {
+                if (cumulativeDeltaTime > 1200)
+                {
+                    previousStrains.RemoveRange(0, i);
+                    break;
+                }
+
+                hardestPreviousDifficulty = Math.Max(hardestPreviousDifficulty, previousStrains[^(i + 1)] * timeDecay(cumulativeDeltaTime));
+
+                cumulativeDeltaTime += current.Previous(i).DeltaTime;
+            }
+
+            return hardestPreviousDifficulty;
         }
     }
 }
